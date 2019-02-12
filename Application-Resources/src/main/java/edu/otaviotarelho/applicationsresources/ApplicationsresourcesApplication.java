8e667eb4685130414a852b553bc8e3eb1f7ec8f7
otaviotarelho <otarelho@ucdavis.edu>
2019-02-11 22:53:45 -0200

    Creating microservices structure

---
+++ Application-Resources/src/main/java/edu/otaviotarelho/applicationsresources/ApplicationsresourcesApplication.java
@@ -0,0 +1,14 @@
+package edu.otaviotarelho.applicationsresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsresourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationsresourcesApplication.class, args);
+    }
+
+}
+
---
+++ Signin-Service/src/main/java/edu/otaviotarelho/signinservice/SigninServiceApplication.java
@@ -0,0 +1,14 @@
+package edu.otaviotarelho.signinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigninServiceApplication.class, args);
+	}
+
+}
+
